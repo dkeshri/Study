ee036b9fa279dbcc804e7b7339e9d69047073e50
dkeshri <dkeshridev@gmail.com>
2023-10-29 00:18:43 +0530

    change program

--- Backend/dotnet/InfluxDb/InfluxDb/Program.cs
+++ Backend/dotnet/InfluxDb/InfluxDb/Program.cs
@@ -1,25 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using InfluxDb;
-
-Console.WriteLine("Hello, World!");
-
-
-
+Console.WriteLine("Press 1 to write data");
+Console.WriteLine("Press 2 to Read data");
+Console.WriteLine("Press 3 to Exit");
+Console.Write("Enter you number : ");
+int ans = Convert.ToInt32(Console.ReadLine());
 InfluxMessageProcesser influxMessageProcesser = new InfluxMessageProcesser();
-
-Message message = new Message()
+while (ans != 3)
 {
-    Tag = "copper",
-    Value = 45D
-};
+    switch (ans)
+    {
+        case 1:
+            Console.Clear();
+            for (int i = 1; i <= 10; i++)
+            {
+                
+                Random rnd = new Random();
+                int num = rnd.Next(1, 100);
+                double value = num / i;
+                Message message = new Message()
+                {
+                    Tag = i%2==0?"India":"USA",
+                    Value = value
+                };
+                Console.WriteLine("Writing Tag: " + message.Tag +" Value: "+message.Value);
+                Task.Delay(500).Wait();
+                
+                influxMessageProcesser.WriteMessage(message);
+            }
+            
+            Console.WriteLine("Data written succesfully");
+            
+            break;
+        case 2:
+            Console.Clear();
+            await influxMessageProcesser.ReadMessage();
+            Console.WriteLine("Data Read succesfully");
+            break;
+        case 3:Environment.Exit(0);
+            break;
+        default:
+            Console.Clear();
+            Console.WriteLine("Please enter correct number");
+            break;
 
-try
-{
-    influxMessageProcesser.WriteMessage(message);
-
-    await influxMessageProcesser.ReadMessage();
+    }
+    Console.WriteLine("Press 1 to write data");
+    Console.WriteLine("Press 2 to Read data");
+    Console.WriteLine("Press 3 to Exit");
+    Console.Write("Enter you number : ");
+    ans = Convert.ToInt32(Console.ReadLine());
 }
-catch(Exception ex)
-{
 
-}
+
+
+
+
