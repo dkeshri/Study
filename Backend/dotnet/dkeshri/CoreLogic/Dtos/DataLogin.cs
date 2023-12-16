04378cd5a3fb8fd90a8aec76b42d7f6ece4feafa
dkeshri <dkeshridev@gmail.com>
2023-12-16 22:28:44 +0530

    solution updated

---
+++ Backend/dotnet/dkeshri/CoreLogic/Dtos/DataLogin.cs
@@ -0,0 +1,17 @@
+﻿using CoreLogic.Data.Interfaces.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLogic.Dtos
+{
+    public class DataLogin
+    {
+        public DataLogin(IAddress address)
+        {
+            
+        }
+    }
+}
---
+++ Backend/dotnet/dkeshri/CoreLogic/Dtos/ServiceLogic.cs
@@ -0,0 +1,17 @@
+﻿using CoreLogic.Service.Interfaces.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLogic.Dtos
+{
+    public class ServiceLogic
+    {
+        public ServiceLogic(IPublicService publicService)
+        {
+                
+        }
+    }
+}
