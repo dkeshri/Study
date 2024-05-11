d52451220bbf1bb001c1f05bd01699bff616f28b
dkeshri <dkeshridev@gmail.com>
2024-05-11 17:06:24 +0530

    RunHosted service

--- Backend/dotnet/IOT/IOTConcept/BackgroundApplication/Program.cs
+++ Backend/dotnet/IOT/IOTConcept/BackgroundApplication/Program.cs
@@ -4,10 +4,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateDefaultBuilder(args);
+
 builder.ConfigureServices((hostContext, services) =>
 {
     services.AddHostedService<MyConsoleService>();
 });
 
-var app = builder.Build();
-app.Run();
+
+builder.RunConsoleAsync().Wait();
