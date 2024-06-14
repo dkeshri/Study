853cd6fe6f120522390aa4a46054713cad72d7cc
dkeshri <dkeshridev@gmail.com>
2024-06-15 00:18:09 +0530

    Test serilog with release app

--- Backend/dotnet/dkeshri/Dkeshri.WebApi/Program.cs
+++ Backend/dotnet/dkeshri/Dkeshri.WebApi/Program.cs
@@ -25,11 +25,11 @@ try
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
-        app.UseSwagger();
-        app.UseSwaggerUI();
+        
     }
 
-    
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
     app.UseHttpsRedirection();
 
