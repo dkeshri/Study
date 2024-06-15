de9a22a45de564ffccd0f445f43cbe992568245a
dkeshri <dkeshridev@gmail.com>
2024-06-15 09:58:54 +0530

    User Enricher

---
+++ Backend/dotnet/dkeshri/Dkeshri.WebApi/UserEnricher.cs
@@ -0,0 +1,23 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace Dkeshri.WebApi
+{
+    public class UserEnricher : ILogEventEnricher
+    {
+        private readonly string _userName;
+        private readonly int _userId;
+
+        public UserEnricher(string userName, int userId)
+        {
+            _userName = userName;
+            _userId = userId;
+        }
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("UserName", _userName));
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("UserId", _userId));
+        }
+    }
+}
