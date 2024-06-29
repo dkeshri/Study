63d93dfe4bb42e8bb8f101314751de12576f825f
dkeshri <dkeshridev@gmail.com>
2024-06-29 19:45:35 +0530

    Controller

---
+++ Backend/dotnet/dkeshri/Dkeshri.WebApi/Controllers/Base/DesignPatternControllerBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dkeshri.WebApi.Controllers.Base
+{
+    [ApiController]
+    [Route("DesignPattern/[controller]")]
+    public class DesignPatternControllerBase : ControllerBase
+    {
+
+    }
+}
---
+++ Backend/dotnet/dkeshri/Dkeshri.WebApi/Controllers/Base/WebApiControllerBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dkeshri.WebApi.Controllers.Base
+{
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class WebApiControllerBase : ControllerBase
+    {
+    }
+}
