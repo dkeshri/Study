b396d994937b1b1a7daf50a8a6f51adcb3e0db35
dkeshri <dkeshridev@gmail.com>
2024-12-27 18:49:01 +0530

    Run in Every 10 Sec

--- Backend/dotnet/IOT/IOTConcept/DataSync/DataSync.DBChangeEmitter/Services/DbChangeEmitterService.cs
+++ Backend/dotnet/IOT/IOTConcept/DataSync/DataSync.DBChangeEmitter/Services/DbChangeEmitterService.cs
@@ -15,7 +15,7 @@ namespace DataSync.DBChangeEmitter.Services
     {
         private int testCounter = 0;
         IDatabaseChangeTrackerService DatabaseChangeTrackerService { get; }
-        public DbChangeEmitterService(IDatabaseChangeTrackerService databaseChangeTrackerService):base(TimeSpan.FromSeconds(4))
+        public DbChangeEmitterService(IDatabaseChangeTrackerService databaseChangeTrackerService):base(TimeSpan.FromSeconds(10))
         {
             DatabaseChangeTrackerService = databaseChangeTrackerService;
         }
@@ -25,9 +25,12 @@ namespace DataSync.DBChangeEmitter.Services
             IReadOnlyCollection<TableChanges> tablechanges = await DatabaseChangeTrackerService.GetChangesOfTrackedTableAsync();
             foreach (TableChanges tablechange in tablechanges)
             {
+                Console.WriteLine($"************ {tablechange.TableName} ************");
+                Console.WriteLine($"Total Records: {tablechange.Records.Count}\n" );
                 foreach (var record in tablechange.Records) {
                     Console.WriteLine(record.Data.ToString());
                 }
+                Console.WriteLine($"\n************ {tablechange.TableName} End ************\n");
             }
             
         }
