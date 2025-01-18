c885bfca3971aae221bb4f2d9b5ccad573421897
dkeshri <dkeshridev@gmail.com>
2025-01-18 09:46:06 +0530

    Update to rabbitMq

---
+++ Backend/dotnet/dkeshri/SystemDesign/ExecuterApp/Services/MessageSenderService.cs
@@ -0,0 +1,24 @@
+﻿using Dkeshri.MessageQueue.Interfaces;
+using ExecuterApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecuterApp.Services
+{
+    internal class MessageSenderService : IMessageSenderService
+    {
+        ISendMessage _sendMessage;
+        public MessageSenderService(ISendMessage sendMessage)
+        {
+            _sendMessage = sendMessage;
+        }
+
+        public void SendMessageToQueue()
+        {
+            _sendMessage.SendToQueue("First Message to Queue");
+        }
+    }
+}
