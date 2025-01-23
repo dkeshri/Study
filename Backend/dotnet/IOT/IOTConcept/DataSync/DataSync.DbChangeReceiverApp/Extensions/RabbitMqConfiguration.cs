088b6268afcc5f0a473986fe892400e8ab7b38c7
dkeshri <dkeshridev@gmail.com>
2025-01-23 12:15:05 +0530

    Update config managemant

--- Backend/dotnet/IOT/IOTConcept/DataSync/DataSync.DbChangeReceiverApp/Extensions/RabbitMqConfiguration.cs
+++ Backend/dotnet/IOT/IOTConcept/DataSync/DataSync.DbChangeReceiverApp/Extensions/RabbitMqConfiguration.cs
@@ -12,12 +12,23 @@
     }
     internal class QueueConfig
     {
+        private string _routingKeys;
         public string Name { get; set; }
         public bool IsDurable { get; set; }
         public bool IsExclusive { get; set; }
         public bool IsAutoDelete { get; set; }
         public string ExchangeName { get; set; }
-        public string[] RoutingKeys { get; set; }
+        public string RoutingKeysCommaSaperated
+        {
+            get => _routingKeys;
+            set => _routingKeys = value; // Setter to set the value of _routingKeys
+        }
+        internal string[] RoutingKeys
+        {
+            get => string.IsNullOrEmpty(_routingKeys)
+                ? Array.Empty<string>()
+                : _routingKeys.Split(',', StringSplitOptions.RemoveEmptyEntries);
+        }
     }
 
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
