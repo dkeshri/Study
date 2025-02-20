cd3e398d2571c2e93558e8c8c8bdb6e0d84d2787
dkeshri <dkeshridev@gmail.com>
2025-02-20 21:16:51 +0530

    Working Ocelot

--- Backend/dotnet/Microservices/ApiGateway/Ocelot/OrderService/WeatherForecast.cs
+++ Backend/dotnet/Microservices/ApiGateway/Ocelot/OrderService/WeatherForecast.cs
@@ -9,5 +9,6 @@ namespace OrderService
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+        public string ServiceName { get; set; } = "OrderService";
     }
 }
