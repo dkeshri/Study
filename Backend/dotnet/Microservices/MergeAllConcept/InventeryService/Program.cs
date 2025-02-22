c04b0465bfffa351937ef555ac241784d2a8e410
dkeshri <dkeshridev@gmail.com>
2025-02-22 22:56:16 +0530

    Update changes

--- Backend/dotnet/Microservices/MergeAllConcept/InventeryService/Program.cs
+++ Backend/dotnet/Microservices/MergeAllConcept/InventeryService/Program.cs
@@ -13,11 +13,8 @@ builder.Services.AddMassTransit();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
