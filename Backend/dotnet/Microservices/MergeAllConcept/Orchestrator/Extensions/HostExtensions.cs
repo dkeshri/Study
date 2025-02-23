92ddf3c4b75b96ebeb42e1945b093261adb8d904
dkeshri <dkeshridev@gmail.com>
2025-02-23 12:58:26 +0530

    Working perfect

---
+++ Backend/dotnet/Microservices/MergeAllConcept/Orchestrator/Extensions/HostExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Orchestrator.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orchestrator.Extensions
+{
+    internal static class HostExtensions
+    {
+        public static bool MigrateDatabase(this IHost host)
+        {
+            bool isMigrationSuccess = false;
+            using (var scope = host.Services.CreateScope())
+            {
+                using (var DataContext = scope.ServiceProvider.GetRequiredService<AppDbContext>())
+                {
+                    try
+                    {
+
+                        if (DataContext.Database.GetPendingMigrations().Any())
+                        {
+                            Console.WriteLine("DB migration started....");
+                            DataContext.Database.Migrate();
+                            isMigrationSuccess = true;
+                            Console.WriteLine("DB migration successfully completed.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("DB migration: Not needed. Already up to date...");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+            }
+            return isMigrationSuccess;
+        }
+    }
+}
